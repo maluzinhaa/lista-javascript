79b580e07edc28fbb894392b64f44cf40f47c62b
Maria Luiza <91751375+maluzinhaa@users.noreply.github.com>
2023-03-20 20:47:38 -0300

    Create ex07.js

---
+++ ex07.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um número inteiro:"));
+
+if (numero % 5 === 0) {
+  console.log(`${numero} é divisível por 5`);
+} else {
+  console.log(`${numero} não é divisível por 5`);
+}
